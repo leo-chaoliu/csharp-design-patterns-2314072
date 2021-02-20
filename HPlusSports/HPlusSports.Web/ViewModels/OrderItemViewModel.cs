0b748dde2d479c3dfe23687fae087e3427a1f5e6
Leo <leo@Leos-MacBook-Pro.local>
2021-02-20 16:29:00 +0800

    Multi interface and extesion usage

--- HPlusSports/HPlusSports.Web/ViewModels/OrderItemViewModel.cs
+++ HPlusSports/HPlusSports.Web/ViewModels/OrderItemViewModel.cs
@@ -4,7 +4,10 @@ using HPlusSports.Models;
 
 namespace HPlusSports.Web.ViewModels
 {
-    public class OrderItemViewModel : Order, IOrderItemViewModel
+    // inherit should be the first in the seq
+    // allow multi interface 
+    // extension can be derived to the model implemented the interface
+    public class OrderItemViewModel : Order, IOrderItemViewModel, IOrderItemViewModel2
     {
         public OrderItemViewModel(IEnumerable<Order> orders, Order o)
         {
@@ -32,6 +35,12 @@ namespace HPlusSports.Web.ViewModels
         public new string TotalDue => base.TotalDue?.ToString("f2");
         public new string OrderDate => base.OrderDate.ToString("d");
         public string PreviousOrderDates { get; private set; }
+
+        public int CustomerId2 => throw new System.NotImplementedException();
+
+        public string CustomerName2 => throw new System.NotImplementedException();
+
+        public string Status2 => throw new System.NotImplementedException();
     }
 
     public interface IOrderItemViewModel
@@ -42,5 +51,20 @@ namespace HPlusSports.Web.ViewModels
         string TotalDue { get; }
         string OrderDate { get; }
         string PreviousOrderDates { get; }
+    }
+
+    public interface IOrderItemViewModel2
+    {
+        int CustomerId2 { get; }
+        string CustomerName2 { get; }
+        string Status2 { get; }
+    }
+
+    // Extension method on interface
+    public static class IOrderItemViewModel2Extension{
+        public static string OrderFullName(this IOrderItemViewModel2 model2){
+            return model2.CustomerName2 + model2.Status2;
+        }
+
     }
 }
